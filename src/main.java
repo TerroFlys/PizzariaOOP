80993439abfdaa7e1b280b9183b10b8b5b31bef1
TerroFlys <35329399+TerroFlys@users.noreply.github.com>
2022-01-10 18:34:25 +0100

    update nested loop

--- src/main.java
+++ src/main.java
@@ -2,5 +2,11 @@ public class main {
     public static void main(String[] args) {
         PizzariaPanel pizzariaPanel = new PizzariaPanel();
         pizzariaPanel.run();
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
